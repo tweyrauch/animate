0048d9b65e6e15f0f993260adac9935d84ceec44
Tia Weyrauch <tiayrock@gmail.com>
2014-04-30 14:39:18 -0700

    Version 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
 // My Scripts
 $(document).foundation();
+
+// Animate.css Stuff
+$('h3').click(function(){
+	// console.log('Click Me')
+	$('img').addClass('animated rubberBand');
+});
+
+// Transit.js Stuff
+$('img').click(function(){
+	$('p').transition({ perspective: '100px',
+  	rotateX: '180deg'});
+});
